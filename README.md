05ce340b2fd4cada1a2ff6018fdeb7fccf3180c3
Chris Lucynski <chris.lucynski@gmail.com>
2019-08-18 14:22:42 -0600

    updated the styling, added readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Train-Scheduler
+
+### What the project does
+
+This app allows the user to add a train to the train schedule. The user will include the name of the train, the train's destination, how frequent the train runs, and when the first train arrives. The app then users that information to display the name, destination, and frequency of the train, as well as the next arrival and how many minutes away the train is. 
+
+### Why the project is useful
+
+This is a great project for those who utilze public transportation, particularly trains, and allows the user to have a quick and easy reference for when their favorite trains run. 
+
+As a developer, this is a great assignment for learning how databases work, how data is sent to the database, and how it is displayed as HTML when it is called from the database. 
+
+
+### How users can get started with the project
+
+Users can start by adding a train in the "add train" section. Once they click submit, the input data is sent to a database (firebase) which then is displayed as HTML in the scheduler above the "add train" section. 
+
+### Who maintains and contributes to the project
+
+I maintain and contributre to this project.
--- assets/app.js
+++ assets/app.js
@@ -19,7 +19,7 @@ $('#submit').click(function(event){
     var frequencyInput = $('#frequency-input').val()
     
     var tFrequency = frequencyInput;
-    var firstTime = moment(firstTrainInput, 'h:mm a').format('h:mm a');
+    var firstTime = moment(firstTrainInput, 'HH:mm').format('HH:mm');
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "h:mm a").subtract(1, "years");
     // Difference between the times
@@ -66,7 +66,16 @@ database.ref().on('child_added', function(snapshot){
 })
 
 
-// issues noted:
+// Things that need to be addressed:
+//                                          First Train Time has to be in military time
+//      Consider updating your "minutes to arrival" and "next train time" text once every minute. This is significantly more challenging; 
+            // only attempt this if you've completed the actual activity and committed it somewhere on GitHub for safekeeping (and maybe create a second GitHub repo).
+//      Try adding update and remove buttons for each train. Let the user edit the row's elements-- 
+            // allow them to change a train's Name, Destination and Arrival Time (and then, by relation, minutes to arrival).
+//      Make it so that only users who log into the site with their Google or GitHub accounts can use your site. 
+            // You'll need to read up on Firebase authentication for this bonus exercise.
+
+
 //      min away does not update, both in real time, as well as when the screen is refreshed
             // have all the input fields push to database, and do timing calculation when we do database.ref()?
 
